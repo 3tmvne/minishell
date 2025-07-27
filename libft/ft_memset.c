1c1326fd457a45351e4d1a0214ad0f5ae02d5ad9
Achraf Regragui <aregragu@c1r7p7.1337.ma>
2025-07-27 23:01:24 +0100

    wertyui

---
+++ libft/ft_memset.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aregragu <aregragu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 18:29:56 by aregragu          #+#    #+#             */
+/*   Updated: 2024/11/09 21:11:22 by aregragu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	str = (unsigned char *)s;
+	while (i < n)
+	{
+		str[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
+
+// int main() {
+// char c = 255;
+//     char str[50] = "future is loading ";
+
+//     printf("bla memset : %s\n", str);
+
+//     ft_memset(str, '-', 6);
+
+//     printf("String mor memset: %s\n", str);
+
+//     return (0);
+// }
