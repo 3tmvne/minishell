a792e0566db644c5fc5cf103567ff4fdf82da46d
Othmane Zemrani <ozemrani@c1r5p3.1337.ma>
2025-08-02 23:06:15 +0100

    fclean

--- syntax_base_simple.c
+++ syntax_base_simple.c
@@ -18,19 +18,20 @@ void	free_tokens(t_token *tokens)
 	}
 }
 
-// Fonction simplifiée pour vérifier l'input de base
+
+//* Fonction simplifiée pour vérifier l'input de base
 int	check_input_syntax(char *input)
 {
 	if (!input || !*input)
 		return (0);
 	
-	// Vérifier si l'entrée contient seulement des espaces
+	//? Vérifier si l'entrée contient seulement des espaces
 	int i = 0;
 	while (input[i] && isspace(input[i]))
 		i++;
 	if (!input[i])
 		return (0);
 
-	// Les quotes et autres vérifications sont gérées par le lexer
+	//? Les quotes et autres vérifications sont gérées par le lexer
 	return (0);
 }
